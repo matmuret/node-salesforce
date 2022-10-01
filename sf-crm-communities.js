45704f3796a72dda3cbf5f3b4f2e14ccc7a7371e
Filippo <filippo.moretti@quadraton.com>
2022-10-01 17:26:52 +0200

    adds sf-crm-communities

---
+++ sf-crm-communities.js
@@ -0,0 +1,37 @@
+const auth = require("./sf-crm-auth");
+
+const fetch = require("node-fetch");
+const { URLSearchParams } = require("url");
+
+const url = "/services/data/v54.0/connect/communities";
+
+module.exports = async function () {
+  const channels = await getCommunitiesList();
+
+  return channels.channels;
+};
+
+async function getCommunitiesList() {
+  return new Promise((resolve, reject) => {
+    auth().then((token) => {
+      const meta = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token.access_token}`,
+      };
+
+      const urlToUse = `${token.instance_url}${url}`;
+      fetch(urlToUse, {
+        method: "GET",
+        headers: meta,
+      })
+        .then((res) => res.json())
+        .then((json) => console.log(json))
+        .then((json) => resolve(json))
+        .catch((err) => {
+          console.log({ err });
+          reject(err);
+        });
+    });
+  });
+}
+getCommunitiesList();
