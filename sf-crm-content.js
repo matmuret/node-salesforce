acc55186f30849be368460e5404329b4d5c4a3d3
Filippo <filippo.moretti@quadraton.com>
2022-10-01 15:36:45 +0200

    fix getContentList(type) in sf-crm-content

--- sf-crm-content.js
+++ sf-crm-content.js
@@ -2,7 +2,7 @@ const auth = require("./sf-crm-auth");
 
 const fetch = require("node-fetch");
 
-const config = require("./sf-crm-config")();
+const config = require("./sf-crm-config");
 
 module.exports = async function (type) {
   if (type) {
@@ -11,30 +11,36 @@ module.exports = async function (type) {
     return content;
   }
 };
+const channelName = "CMSExample";
+getContentList("Image");
 
 async function getContentList(type) {
   return new Promise((resolve, reject) => {
     auth().then((token) => {
-      const meta = {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token.access_token}`,
-      };
+      config().then((channels) => {
+        return channels.filter((channel) => {
+          if (channel.channelName === channelName) {
+            const meta = {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token.access_token}`,
+            };
 
-      const urlToUse = `${token.instance_url}/services/data/v54.0/connect/cms/delivery/channels/${config.channelId}/contents
-/query?managedContentType=${type}&pageSize=250`;
-
-      fetch(urlToUse, {
-        method: "GET",
-        headers: meta,
-      })
-        .then((res) => res.json())
-        //.then((json) => console.log(json))
-        .then((json) => resolve(json))
-        .catch((err) => {
-          console.log({ err });
-          reject(err);
+            const urlToUse = `${token.instance_url}/services/data/v54.0/connect/cms/delivery/channels/${channel.channelId}/contents/query?managedContentType=${type}&pageSize=250`;
+            console.log(urlToUse);
+            fetch(urlToUse, {
+              method: "GET",
+              headers: meta,
+            })
+              .then((res) => res.json())
+              .then((json) => console.log(json))
+              .then((json) => resolve(json))
+              .catch((err) => {
+                console.log({ err });
+                reject(err);
+              });
+          }
         });
+      });
     });
   });
 }
-getContentList("");
